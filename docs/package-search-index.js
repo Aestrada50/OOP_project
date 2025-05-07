2b50bbe22ddea653a676c1b968089b64095bc4c0
Aestrada50 <agestra217@gmail.com>
2025-05-06 23:37:16 -0600

    f

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"auth"},{"l":"factory"},{"l":"log"},{"l":"menu"},{"l":"model"}];updateSearchResults();
